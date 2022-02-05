409e8b6e80b97940d32b636cc76265ea9448c18f
ross-k <mymac@MyMacs-MacBook-Pro.local>
2022-02-05 13:36:29 -0600

    Finished everything besides the pesky 2c.

--- SQL_Prescribers_2.sql
+++ SQL_Prescribers_2.sql
@@ -18,10 +18,28 @@ FROM
 ) npi_both;
 
 
+	-- HABEEB HAD
+	SELECT 
+		(SELECT COUNT(DISTINCT npi) AS npi_count
+		FROM prescriber) - 
+		(SELECT COUNT(DISTINCT npi) AS npi_count
+		FROM prescription) AS npi_difference
+
+
+	-- JESSICA HAD
+	SELECT COUNT(npi)
+	FROM prescriber
+	WHERE NOT EXISTS 
+	(
+    	SELECT npi
+    	FROM prescription
+    	WHERE prescriber.npi = prescription.npi
+	);
+	
 /*
 	2. a. Find the top five drugs (generic_name) prescribed by prescribers with the specialty of Family Practice.
 	
-	A - LEVOTHYROXINE SODIUM; LISINOPRIL; ATORVASTATIN CALCIUM; AMLODIPINE BESYLATE; AND OMEPRAZOLE.
+	A - LEVOTHYROXINE SODIUM, LISINOPRIL, ATORVASTATIN CALCIUM, AMLODIPINE BESYLATE, and OMEPRAZOLE.
 */
 
 SELECT DISTINCT d.generic_name,
@@ -40,7 +58,7 @@ LIMIT 5;
 /*
     b. Find the top five drugs (generic_name) prescribed by prescribers with the specialty of Cardiology.
 	
-	A - ATORVASTATIN CALCIUM; CARVEDILOL; METOPROLOL TARTRATE; CLOPIDOGREL BISULFATE; AND AMLODIPINE BESYLATE.
+	A - ATORVASTATIN CALCIUM, CARVEDILOL, METOPROLOL TARTRATE, CLOPIDOGREL BISULFATE, and AMLODIPINE BESYLATE.
 */
 
 SELECT DISTINCT d.generic_name,
@@ -58,7 +76,115 @@ LIMIT 5;
 
 /*
     c. Which drugs appear in the top five prescribed for both Family Practice prescribers and Cardiologists? Combine what you did for parts a and b into a single query to answer this question.
+	
+	A - ATORVASTATIN CALCIUM, LEVOTHYROXINE SODIUM, AMLODIPINE BESYLATE, LISINOPRIL, and FUROSEMIDE.
 */
+
+	-- tmp TABLE WAY STILL NOT WORKING
+	CREATE TEMP TABLE tmp_card
+	(
+		generic_name text,
+		total_claims numeric
+	);
+	
+	CREATE TEMP TABLE tmp_fam
+	(
+		generic_name text,
+		total_claims numeric
+	);
+
+	BEGIN TRANSACTION;
+	INSERT INTO tmp_card
+	SELECT DISTINCT d.generic_name,
+		COALESCE(SUM(pn.total_claim_count),0) AS total_claims
+	FROM drug d
+		INNER JOIN prescription pn
+			ON d.drug_name = pn.drug_name
+		INNER JOIN prescriber pr
+			ON pr.npi = pn.npi
+	WHERE pr.specialty_description = 'Cardiology'
+	GROUP BY d.generic_name
+	ORDER BY total_claims DESC
+	LIMIT 5;
+	COMMIT TRANSACTION;
+	
+
+	BEGIN TRANSACTION;
+	INSERT INTO tmp_fam
+	SELECT DISTINCT d.generic_name,
+		COALESCE(SUM(pn.total_claim_count),0) AS total_claims
+	FROM drug d
+		INNER JOIN prescription pn
+			ON d.drug_name = pn.drug_name
+		INNER JOIN prescriber pr
+			ON pr.npi = pn.npi
+	WHERE pr.specialty_description = 'Family Practice'
+	GROUP BY d.generic_name
+	ORDER BY total_claims DESC
+	LIMIT 5;
+	COMMIT TRANSACTION;
+	
+	
+	SELECT *
+	FROM tmp_card
+	INTERSECT
+	SELECT *
+	FROM tmp_fam
+	ORDER BY generic_name
+	;
+	
+	
+
+	-- THIS DOES NOT GENERATE THE RIGHT RESULTS
+	SELECT DISTINCT d.generic_name AS fam_prac_name,
+		COALESCE(SUM(pn.total_claim_count),0) AS fam_prac_claims
+	FROM drug d
+		INNER JOIN prescription pn
+			ON d.drug_name = pn.drug_name
+		INNER JOIN prescriber pr
+			ON pr.npi = pn.npi
+	WHERE pr.specialty_description IN 
+		('Family Practice', 'Cardiology')
+	GROUP BY d.generic_name
+	ORDER BY fam_prac_claims DESC
+	LIMIT 5;
+
+
+
+
+-- WHY DOES THIS NOT WORK?
+SELECT *	-- USING BRYAN'S TRICK FROM SQL_Prescribers Question 5b)
+FROM 
+(
+	SELECT DISTINCT d.generic_name AS fam_prac_name,
+		COALESCE(SUM(pn.total_claim_count),0) AS fam_prac_claims
+	FROM drug d
+		INNER JOIN prescription pn
+			ON d.drug_name = pn.drug_name
+		INNER JOIN prescriber pr
+			ON pr.npi = pn.npi
+	WHERE pr.specialty_description = 'Family Practice'
+	GROUP BY d.generic_name
+	ORDER BY fam_prac_claims DESC
+	LIMIT 5
+) sq1
+INTERSECT
+SELECT *
+FROM 
+(
+	SELECT DISTINCT d.generic_name AS card_name,
+		COALESCE(SUM(pn.total_claim_count),0) AS card_claims
+	FROM drug d
+		INNER JOIN prescription pn
+			ON d.drug_name = pn.drug_name
+		INNER JOIN prescriber pr
+			ON pr.npi = pn.npi
+	WHERE pr.specialty_description = 'Cardiology'
+	GROUP BY d.generic_name
+	ORDER BY card_claims DESC
+	LIMIT 5	
+) sq2;
+
 
 
 -- TO DO - RESEARCH WHY THIS DOES NOT WORK
@@ -124,7 +250,7 @@ FROM cteCard
 
 
 /*
-3. Your goal in this question is to generate a list of the top prescribers in each of the major metropolitan areas of Tennessee.
+	3. Your goal in this question is to generate a list of the top prescribers in each of the major metropolitan areas of Tennessee.
 
     a. First, write a query that finds the top 5 prescribers in Nashville in terms of the total number of claims (total_claim_count) across all drugs. Report the npi, the total number of claims, and include a column showing the city.
 */
@@ -163,7 +289,119 @@ FROM cteCard
 	c. Combine your results from a and b, along with the results for Knoxville and Chattanooga.
 */
 
-	-- TO DO - RESEARCH A BETTER WAY TO DO THIS
+-- tmp TABLE VERSION
+CREATE TEMP TABLE tmp_Chattanooga
+(
+	npi numeric,
+	nppes_provider_city text,
+	total_claims numeric
+);
+
+CREATE TEMP TABLE tmp_Knoxville
+(
+	npi numeric,
+	nppes_provider_city text,
+	total_claims numeric
+);
+
+CREATE TEMP TABLE tmp_Memphis
+(
+	npi numeric,
+	nppes_provider_city text,
+	total_claims numeric
+);
+
+CREATE TEMP TABLE tmp_Nashville
+(
+	npi numeric,
+	nppes_provider_city text,
+	total_claims numeric
+);
+
+BEGIN TRANSACTION;
+INSERT INTO tmp_Chattanooga
+SELECT DISTINCT pr.npi,
+	pr.nppes_provider_city,
+	COALESCE(SUM(pn.total_claim_count),0) AS total_claims
+FROM prescriber pr
+	INNER JOIN prescription pn
+		ON pr.npi = pn.npi
+WHERE nppes_provider_city = 'CHATTANOOGA'
+GROUP BY pr.npi,
+	pr.nppes_provider_city
+ORDER BY total_claims DESC
+LIMIT 5;
+COMMIT TRANSACTION;
+
+
+BEGIN TRANSACTION;
+INSERT INTO tmp_Knoxville
+SELECT DISTINCT pr.npi,
+	pr.nppes_provider_city,
+	COALESCE(SUM(pn.total_claim_count),0) AS total_claims
+FROM prescriber pr
+	INNER JOIN prescription pn
+		ON pr.npi = pn.npi
+WHERE nppes_provider_city = 'KNOXVILLE'
+GROUP BY pr.npi,
+	pr.nppes_provider_city
+ORDER BY total_claims DESC
+LIMIT 5;
+COMMIT TRANSACTION;
+
+
+BEGIN TRANSACTION;
+INSERT INTO tmp_Memphis
+SELECT DISTINCT pr.npi,
+	pr.nppes_provider_city,
+	COALESCE(SUM(pn.total_claim_count),0) AS total_claims
+FROM prescriber pr
+	INNER JOIN prescription pn
+		ON pr.npi = pn.npi
+WHERE nppes_provider_city = 'MEMPHIS'
+GROUP BY pr.npi,
+	pr.nppes_provider_city
+ORDER BY total_claims DESC
+LIMIT 5;
+COMMIT TRANSACTION;
+
+
+BEGIN TRANSACTION;
+INSERT INTO tmp_Nashville
+SELECT DISTINCT pr.npi,
+	pr.nppes_provider_city,
+	COALESCE(SUM(pn.total_claim_count),0) AS total_claims
+FROM prescriber pr
+	INNER JOIN prescription pn
+		ON pr.npi = pn.npi
+WHERE nppes_provider_city = 'NASHVILLE'
+GROUP BY pr.npi,
+	pr.nppes_provider_city
+ORDER BY total_claims DESC
+LIMIT 5;
+COMMIT TRANSACTION;
+
+
+SELECT *
+FROM tmp_Chattanooga
+UNION 
+SELECT *
+FROM tmp_Knoxville
+UNION 
+SELECT *
+FROM tmp_Memphis
+UNION 
+SELECT *
+FROM tmp_Nashville
+ORDER BY nppes_provider_city,
+	npi
+;
+
+
+	-- MAYBE RESEARCH pivot OPTION??
+
+
+	-- cte UNION VERSION NOT WORKING - TO DO - RESEARCH A BETTER WAY TO DO THIS
 	SELECT DISTINCT pr.npi,
 		pr.nppes_provider_city,
 		COALESCE(SUM(pn.total_claim_count),0) AS total_claims
@@ -176,7 +414,7 @@ FROM cteCard
 	ORDER BY total_claims DESC
 	LIMIT 5
 	UNION
-		SELECT DISTINCT pr.npi,
+	SELECT DISTINCT pr.npi,
 		pr.nppes_provider_city,
 		COALESCE(SUM(pn.total_claim_count),0) AS total_claims
 	FROM prescriber pr
@@ -188,7 +426,7 @@ FROM cteCard
 	ORDER BY total_claims DESC
 	LIMIT 5
 	UNION
-		SELECT DISTINCT pr.npi,
+	SELECT DISTINCT pr.npi,
 		pr.nppes_provider_city,
 		COALESCE(SUM(pn.total_claim_count),0) AS total_claims
 	FROM prescriber pr
@@ -199,7 +437,8 @@ FROM cteCard
 		pr.nppes_provider_city
 	ORDER BY total_claims DESC
 	LIMIT 5
-		SELECT DISTINCT pr.npi,
+	UNION
+	SELECT DISTINCT pr.npi,
 		pr.nppes_provider_city,
 		COALESCE(SUM(pn.total_claim_count),0) AS total_claims
 	FROM prescriber pr
@@ -249,6 +488,7 @@ FROM cteCard
 	5. a. Write a query that finds the total population of Tennessee.
 */	
 
+-- FIRST PASS OF TO_CHAR(123456.78, 'fm999G999D99') NOT WORKING - SEE https://database.guide/format-numbers-with-commas-in-postgresql/ 
 SELECT SUM(population) AS TN_total_pop
 FROM population
 WHERE fipscounty IN
@@ -262,9 +502,6 @@ WHERE fipscounty IN
 /*
     b. Build off of the query that you wrote in part a to write a query that returns for each county that county's name, its population, and the percentage of the total population of Tennessee that is contained in that county.
 */
-
-		TO DO!
-
 
 ;WITH cteFips AS
 (	
@@ -284,10 +521,10 @@ cteTotalPop AS
 	FROM cteFips
 )
 SELECT DISTINCT fc.county,
-	ctfp.fips_pop,
-	ctfp.fips_pop / 
-		(SELECT SUM(fips_pop)
-		 FROM cteTotalPop ctp) AS Population_Pct
+	ctfp.fips_pop AS County_Pop,
+	(SELECT ROUND(ctfp.fips_pop * 100 / 
+		(SELECT TN_total_pop
+		 FROM cteTotalPop ctp), 2)) AS Pct_of_State_Pop
 FROM fips_county fc
 	INNER JOIN cteFips ctfp
 		ON fc.fipscounty = ctfp.fips_code
